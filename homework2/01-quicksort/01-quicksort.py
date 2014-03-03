84e53e5d8a99f35391ba7dfa8437a5b7d41047ac
Jim Blaney <jb38@hood.edu>
2014-03-02 19:15:59 -0500

    finished quicksort implementation

--- homework2/01-quicksort/01-quicksort.py
+++ homework2/01-quicksort/01-quicksort.py
@@ -23,59 +23,61 @@ class MiddlePivotSelector:
         return int((start + end) / 2);
     def Name(self):
         return "Middle";
+
+class RandomPivotSelector:
+    def GetPivot(self, theList, start, end):
+        random_int = int(os.urandom(4).encode('hex'), 16);
+        random_int %= (end - start);
+        random_int += start;
+        return random_int;
+    def Name(self):
+        return "Random";
+
+class MedianPivotSelector:
+    def GetPivot(self, theList, start, end):
+        mid = int((start + end) / 2)
+        list = [theList[start], theList[mid], theList[end]];
+        if (theList[start] > theList[mid]):
+            if (theList[mid] > theList[end]):
+                return mid;
+            elif (theList[start] > theList[end]):
+                return end;
+            else:
+                return start;
+        else:
+            if (theList[start] > theList[end]):
+                return start;
+            elif (theList[mid] > theList[end]):
+                return end;
+            else:
+                return mid;
+        return start;
+    def Name(self):
+        return "Median";
         
 class Quicksort():
     def __init__(self, p):
         self.pivot_selector = p;
 
-    def Sort(self, theList, start, end):
-        print "::Sort(%i -> %i)" % (start, end);
-        print theList;
-        
-        if start < end:
-            # partition the list
-            pivot = self.Partition(theList, start, end);
-            # sort both halves
-            self.Sort(theList, start, pivot - 1);
-            self.Sort(theList, pivot + 1, end);
-        return theList;
-        
-    def Partition(self, theList, start, end):
-        pivot_index = self.pivot_selector.GetPivot(theList, start, end);
-        pivot = theList[pivot_index];
-        
-        print "::Partition(%i -> %i)" % (start, end);
-        print theList;
-        
-        print "Pivot Index: %i (%i)" % (pivot_index, pivot);
-        
-        left = start + 1;
-        right = end;
-        done = False;
-
-        while not done:
-            while left <= right and theList[left] <= pivot:
-                left += 1;
-
-            while theList[right] >= pivot and right >=left:
-                right -= 1;
-
-            if right < left:
-                done = True;
-            else:
-                # swap places
-                temp = theList[left];
-                theList[left] = theList[right];
-                theList[right] = temp;
-
-        # swap start with theList[right]
-        temp = theList[start];
-        theList[start] = theList[right];
-        theList[right] = temp;
-
-        print "Right: %i" % (right);
-        
-        return right;
+    def Sort(self, arr):
+        less = [];
+        pivot_list = [];
+        more = [];
+        if len(arr) <= 1:
+            return arr;
+        else:
+            pivot_index = self.pivot_selector.GetPivot(arr, 0, len(arr) - 1);
+            pivot = arr[pivot_index]
+            for i in arr:
+                if i < pivot:
+                    less.append(i);
+                elif i > pivot:
+                    more.append(i);
+                else:
+                    pivot_list.append(i);
+            less = self.Sort(less);
+            more = self.Sort(more);
+            return less + pivot_list + more;
 
 def ReadFile(file):
     f = open(file, "r");
@@ -86,25 +88,40 @@ def ReadFile(file):
         if len(token) > 0:
             arr.append(int(token));
     return arr;
+
+def WriteFile(file, arr):
+    f = open(file, "w");
+    f.write(" ".join(str(x) for x in arr));
+    f.close();
         
 if __name__ == "__main__":
-    print "Count\tStrategy\tTime";
+    print "Count\tStrategy\tFormat\tTime";
     pivot_selectors = [
-      FirstPivotSelector(),
-      LastPivotSelector(),
-      MiddlePivotSelector()
+        FirstPivotSelector(),
+        LastPivotSelector(),
+        MiddlePivotSelector(),
+        RandomPivotSelector(),
+        MedianPivotSelector()
     ];
     for pivot_selector in pivot_selectors:
+        count = 0;
         for f in sorted(os.listdir("data")):
             arr = ReadFile("data/" + f);
             q = Quicksort(pivot_selector);
             start_time = time.time();
-            q.Sort(arr, 0, len(arr) - 1);
+            arr = q.Sort(arr);
             end_time = time.time();
-            print arr;
-            print("%i\t%s\t%g" % (len(arr), pivot_selector.Name(), end_time - start_time));
-            break;
-        break;
+            #print arr;
+            #WriteFile("data-sorted/" + f, arr); #bootstrap for generating sorted files
+            print("%i\t%s\tUnsorted\t%g" % (len(arr), pivot_selector.Name(), end_time - start_time));
+        for f in sorted(os.listdir("data-sorted")):
+            arr = ReadFile("data-sorted/" + f);
+            q = Quicksort(pivot_selector);
+            start_time = time.time();
+            arr = q.Sort(arr);
+            end_time = time.time();
+            #print arr;
+            print("%i*\t%s\tSorted\t%g" % (len(arr), pivot_selector.Name(), end_time - start_time));
     #a = Andy();
     #g = Quicksort(Andy());
     #g.Partition("lab on fire");
