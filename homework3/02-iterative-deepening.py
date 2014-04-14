55d7d732b8c31c7f1b53a42a8ad2638d54e5ed61
Jim Blaney <jb38@hood.edu>
2014-04-14 01:11:19 -0400

    1 done 2 in progress

---
+++ homework3/02-iterative-deepening.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+class Graph:
+    def __init__(self, num_vertices = 0):
+        self.p = [[0 for x in xrange(num_vertices)] for x in xrange(num_vertices)];
+
+    def is_edge(self, i, j):
+        return self.p[i][j] != 0;
+
+    def set_edge(self, i, j, value):
+        self.p[i][j] = value;
+
+    def num_nodes(self):
+        return len(self.p[0]);
+
+    def __str__(self):
+        return str(self.p);
+
+def IDS(graph, vertex, depth = 2, visited = None):
+
+    if visited is None:
+      visited = [False for x in xrange(graph.num_nodes())];
+
+    visited[vertex] = True;
+
+    print str(vertex),
+
+    if depth > 0:
+      for w in xrange(g.num_nodes()):
+          if w is vertex:
+              continue;
+          if g.is_edge(vertex, w) and not visited[w]:
+              IDS(graph, w, depth - 1, visited);
+
+def ReadFile(file):
+    f = open(file, 'r');
+    lines = f.readlines();
+    f.close();
+    arrs = [];
+    for line in lines:
+        arr = [];
+        for token in line.split(','):
+            if len(token) > 0:
+                arr.append(int(token));
+        arrs.append(arr);
+    return arrs;
+
+if __name__ == '__main__':
+    # read the input
+    data = ReadFile('input.txt');
+    # create the data structure
+    num_vertices = len(data[0]);
+    g = Graph(num_vertices);
+    # populate the graph
+    for i in xrange(num_vertices):
+        for j in xrange(num_vertices):
+            g.set_edge(i, j, data[i][j]);
+    # perform the DFS
+    IDS(g, 0);
